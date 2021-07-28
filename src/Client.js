ce99ea77c9102b07551e345d676c9f2410051e0c
yuko1101 <68993883+yuko1101@users.noreply.github.com>
2021-07-28 19:45:27 +0900

    set args to object

--- src/Client.js
+++ src/Client.js
@@ -15,10 +15,29 @@ module.exports = class Client {
                 if (!msg.content.startsWith(prefix.toLowerCase())) continue
                 const args = msg.content.toLowerCase().replace(prefix.toLowerCase(), "").split(/ +/)
                 const cmd = args.shift()
+                const argsObject = argsToObject(args, command.args)
                 if (command.name && cmd.toLowerCase() !== command.name.toLowerCase()) return
-                return await command.run(msg, args, this.client)
+                return await command.run(msg, argsObject, this.client)
             }
 
         })
     }
+}
+
+function argsToObject(args, msgArgsOption) {
+    const argObj = {}
+    if (args.length <= msgArgsOption.length) {
+        args.forEach((arg, i, array) => argObj[msgArgsOption[i]] = arg)
+        return argObj
+    } else if (args.length > msgArgsOption.length) {
+        for (var i = 0; i < msgArgsOption.length; i++) {
+            if (i === msgArgsOption.length - 1) {
+                argObj[msgArgsOption[i]] = args.join(" ")
+                break
+            }
+            argObj[msgArgsOption[i]] = args.shift()
+        }
+        return argObj
+    }
+    return argObj
 }
