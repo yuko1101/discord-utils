f6724ba6e3e24156a576c4b3e7fe7de234a7dfe2
yuko1101 <68993883+yuko1101@users.noreply.github.com>
2021-08-29 09:29:40 +0900

    update for interaction options

--- src/handlers/Handler.js
+++ src/handlers/Handler.js
@@ -5,36 +5,29 @@ const Discord = require("discord.js")
 
 module.exports = {
     slashCommand: (utilsClient) => {
-        utilsClient.client.ws.on('INTERACTION_CREATE', async interaction => {
-            if (interaction.type === 2) {
-                if (utilsClient.debugGuild && !interaction.data.name.endsWith("-debug")) return
+        utilsClient.client.on("interactionCreate", async interaction => {
+            if (interaction.isCommand()) {
+                if (utilsClient.debugGuild && !interaction.commandName.endsWith("-debug")) return
                 utilsClient.debug(interaction)
-                const cmd = utilsClient.debugGuild ? getSimpleCommandName(interaction.data.name).toLowerCase() : interaction.data.name.toLowerCase();
+                const cmd = utilsClient.debugGuild ? getSimpleCommandName(interaction.commandName).toLowerCase() : interaction.commandName.toLowerCase();
 
                 const args = {}
-                if (interaction.data.options) interaction.data.options.forEach(arg => args[arg.name] = arg.value);
+                if (interaction.options) interaction.options.data.forEach(arg => args[arg.name] = arg.value);
 
                 var command = utilsClient.client.commands.get(cmd)
                 if (!command) command = utilsClient.client.commands.get(utilsClient.client.aliases.get(cmd))
                 if (!command) return
 
-                const msg = interaction
-                msg.slashCommand = true
-                msg.author = interaction.member.user
-                msg.guild = await utilsClient.client.guilds.fetch(interaction.guild_id)
-                msg.channel = await utilsClient.client.channels.fetch(interaction.channel_id)
+                const msg = interaction;
+                msg.slashCommand = true;
+                msg.author = interaction.user;
 
-                const callback = await command.run(msg, args, utilsClient.client)
-                utilsClient.debug(callback)
+                const callback = await command.run(msg, args, utilsClient.client);
+                utilsClient.debug(callback);
                 //if you return null or undefined in Command run function, runAfter won't be triggered
-                if (callback === null) return
+                if (callback === null) return;
                 if (callback === undefined) {
-                    await utilsClient.client.api.interactions(interaction.id, interaction.token).callback.post({
-                        data: {
-                            type: 5
-                        }
-                    })
-                    return
+                    await interaction.deferReply();
                 }
                 var data = {
                     content: callback
@@ -42,17 +35,65 @@ module.exports = {
                 if (typeof callback === "object") {
                     data = callback
                 }
-                await utilsClient.client.api.interactions(interaction.id, interaction.token).callback.post({
-                    data: {
-                        type: 4,
-                        data: data
+                const interactionMessage = await getInteractionMessage(utilsClient.client, interaction, utilsClient.application_id)
+                if (callback !== undefined) await interaction.reply(data); else {
+                    interactionMessage.edit = async (data) => {
+                        await interaction.editReply(data);
                     }
-                })
-                if (command.runAfter) command.runAfter(msg, await getInteractionMessage(utilsClient.client, interaction, utilsClient.application_id), args, utilsClient.client)
+                };
+                if (command.runAfter) command.runAfter(msg, interactionMessage, args, utilsClient.client);
 
             }
-
         })
+
+        // utilsClient.client.ws.on('INTERACTION_CREATE', async interaction => {
+        //     if (interaction.type === 2) {
+        //         if (utilsClient.debugGuild && !interaction.data.name.endsWith("-debug")) return
+        //         utilsClient.debug(interaction)
+        //         const cmd = utilsClient.debugGuild ? getSimpleCommandName(interaction.data.name).toLowerCase() : interaction.data.name.toLowerCase();
+
+        //         const args = {}
+        //         if (interaction.data.options) interaction.data.options.forEach(arg => args[arg.name] = arg.value);
+
+        //         var command = utilsClient.client.commands.get(cmd)
+        //         if (!command) command = utilsClient.client.commands.get(utilsClient.client.aliases.get(cmd))
+        //         if (!command) return
+
+        //         const msg = interaction
+        //         msg.slashCommand = true
+        //         msg.author = interaction.member.user
+        //         msg.guild = await utilsClient.client.guilds.fetch(interaction.guild_id)
+        //         msg.channel = await utilsClient.client.channels.fetch(interaction.channel_id)
+
+        //         const callback = await command.run(msg, args, utilsClient.client)
+        //         utilsClient.debug(callback)
+        //         //if you return null or undefined in Command run function, runAfter won't be triggered
+        //         if (callback === null) return
+        //         if (callback === undefined) {
+        //             await utilsClient.client.api.interactions(interaction.id, interaction.token).callback.post({
+        //                 data: {
+        //                     type: 5
+        //                 }
+        //             })
+        //             return
+        //         }
+        //         var data = {
+        //             content: callback
+        //         }
+        //         if (typeof callback === "object") {
+        //             data = callback
+        //         }
+        //         await utilsClient.client.api.interactions(interaction.id, interaction.token).callback.post({
+        //             data: {
+        //                 type: 4,
+        //                 data: data
+        //             }
+        //         })
+        //         if (command.runAfter) command.runAfter(msg, await getInteractionMessage(utilsClient.client, interaction, utilsClient.application_id), args, utilsClient.client)
+
+        //     }
+
+        // })
     },
     messageCommand: (utilsClient) => {
         utilsClient.client.on("messageCreate", async msg => {
@@ -85,10 +126,9 @@ module.exports = {
 }
 
 async function getInteractionMessage(client, interaction, application_id) {
-    const channel = await client.channels.resolve(interaction.channel_id);
     return await fetch(`https://discord.com/api/v8/webhooks/${application_id}/${interaction.token}/messages/@original`).then(res => res.json()).then(async res => {
         console.log(res)
-        return await channel.messages.fetch(res.id)
+        return await interaction.channel.messages.fetch(res.id)
     })
 };
 
