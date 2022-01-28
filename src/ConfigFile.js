5c2869ae18c583915b2cedd88168ae1d079af7bd
yuko1101 <68993883+yuko1101@users.noreply.github.com>
2022-01-28 10:49:48 +0900

    remake

--- src/ConfigFile.js
+++ src/ConfigFile.js
@@ -1,104 +1,163 @@
-const fs = require("fs")
-const path = require("path")
-const { hasPath, createPath } = require("./utils/helper")
+const fs = require('fs');
 
 module.exports = class ConfigFile {
-    constructor(file_path, compact = false) {
-        this.file_path = file_path
-        this.isReady = false
-        this.resolevedPath = path.resolve(require.main.path, file_path)
-        this.raw = null
-        this.compact = compact
-        fs.readFile(this.resolevedPath, { encoding: "utf8" }, (err, file) => {
-            if (err) {
-                throw new Error(err)
-            } else {
-                try {
-                    console.log(file)
-                    this.raw = JSON.parse(file)
-                    this.isReady = true
-                } catch (e) {
-                    throw new Error(e)
-                }
-                if (typeof this.raw !== "object") throw new Error("You can save only JSON Object in ConfigFile")
-            }
-        })
-    }
-
-    async reload() {
-        if (!this.isReady) throw new Error("ConfigFile is not ready")
-        try {
-            this.raw = JSON.parse(fs.readFileSync(this.resolevedPath))
-
-        } catch (e) {
-            throw new Error(e)
-        }
-        if (typeof this.raw !== "object") throw new Error("You can save only JSON Object in ConfigFile")
-        return this.raw
+    constructor(filePath, defaultConfig, route = []) {
+        this.filePath = filePath;
+        this.defaultConfig = defaultConfig;
+        this.data = defaultConfig;
+        this.route = route;
     }
 
     /**
-     * @param {Object} data
+     * @param {Boolean} [compact=false]
+     * @returns {Promise<ConfigFile>}
      */
-    set json(data) {
-        console.log(typeof data)
-        if (typeof data !== "object") throw new Error("You can save only JSON Object in ConfigFile")
-        this.updateFile()
-        this.raw = data
-    }
-
-    get json() {
-        return this.raw
+    async save(compact = false) {
+        var text;
+        if (compact) {
+            text = JSON.stringify(this.data);
+        } else {
+            text = JSON.stringify(this.data, null, 4);
+        }
+        fs.writeFileSync(this.filePath, text);
+        return this;
     }
 
     /**
-     * @param {String} key 
+     * @returns {Promise<ConfigFile>}
+     */
+    async load() {
+        if (!fs.existsSync(this.filePath)) {
+            await this.save();
+        }
+        const text = fs.readFileSync(this.filePath, "utf-8");
+        try {
+            this.data = JSON.parse(text);
+        } catch (e) {
+            console.error(e);
+        }
+        return this;
+    }
+
+    /**
+     * @param {String|Number} key
      * @param {*} value
+     * @returns {ConfigFile}
      */
     set(key, value) {
-        if (!this.isReady) throw new Error("ConfigFile is not ready")
-        if (typeof this.json !== "object") throw new Error("You can save only JSON Object in ConfigFile")
-        const keys = key.split(".")
-        if (!hasPath(this.json, ...keys)) createPath(this.json, ...keys)
-        var data = this.json
-        for (var i = 0; i < keys.length; i++) {
-            if (i !== keys.length - 1) {
-                data = data[keys[i]]
+        if (typeof key === "number" && !Array.isArray(this.getCurrentRouteObject())) {
+            if (this.route.length === 0) {
+                this.data = [];
             } else {
-                data[keys[i]] = value
+                this.getParentRouteObject()[this.route[this.route.length - 1]] = [];
             }
-
         }
-
-        this.updateFile()
+        this.getCurrentRouteObject()[key] = value;
+        return this
     }
 
     /**
-     * @param {String} key 
-     * @param {*} default_value
+     * @param {*} value
+     * @returns {ConfigFile}
      */
-    get(key, default_value) {
-        if (!this.isReady) throw new Error("ConfigFile is not ready")
-        if (typeof this.json !== "object") throw new Error("You can save only JSON Object in ConfigFile")
-        const keys = key.split(".")
-        if (!hasPath(this.json, ...keys)) return default_value
-        var data = this.json
-        for (var i = 0; i < keys.length; i++) {
-            if (i !== keys.length - 1) {
-                data = data[keys[i]]
+    add(value) {
+        if (this.getCurrentRouteObject() === undefined) {
+            if (this.route.length === 0) {
+                this.data = [value];
             } else {
-                return data[keys[i]]
+                this.getParentRouteObject()[this.route[this.route.length - 1]] = [value];
             }
-
+        } else if (Array.isArray(this.getCurrentRouteObject())) {
+            if (this.route.length === 0) {
+                this.data = [...this.data, value];
+            }
+            else {
+                this.getParentRouteObject()[this.route[this.route.length - 1]] = [...this.getParentRouteObject()[this.route[this.route.length - 1]], value];
+            }
         }
+        return this;
     }
 
-    async updateFile() {
-        try {
-            fs.writeFileSync(this.resolevedPath, this.compact ? JSON.stringify(this.raw) : JSON.stringify(this.raw, null, "\t"))
-        } catch (e) {
-            throw new Error(e)
+    /**
+     * @param {String|Number} key
+     * @returns {*}
+     */
+    getValue(key = null) {
+        if (key === null) {
+            return this.getParentRouteObject()[this.route[this.route.length - 1]];
         }
-        return
+        return this.getCurrentRouteObject()[key];
     }
+
+    /**
+     * @param {String|Number} key
+     * @returns {ConfigFile}
+     */
+    get(...key) {
+        const newRoute = [...this.route, ...key];
+        return new ConfigFile(this.filePath, this.data, newRoute);
+    }
+
+    /**
+     * 
+     * @param  {...String|Number} key 
+     * @returns {Boolean}
+     */
+    has(...key) {
+        const newRoute = [...this.route, ...key];
+        var obj = this.data;
+        for (var i = 0; i < newRoute.length; i++) {
+            obj = obj[newRoute[i]];
+            if (obj === undefined) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * @returns {Boolean}
+     */
+    exists() {
+        return this.has(...this.route);
+    }
+
+    /**
+     * @returns {ConfigFile}
+     */
+    resetData() {
+        this.data = this.defaultConfig;
+        return this;
+    }
+
+    /**
+     * @returns {ConfigFile}
+     */
+    resetPath() {
+        this.route = [];
+        return this;
+    }
+
+
+    /**
+     * @private
+     */
+    getCurrentRouteObject() {
+        var obj = this.data;
+        for (var i = 0; i < this.route.length; i++) {
+            obj = obj[this.route[i]];
+        }
+        return obj;
+    }
+    /**
+     * @private
+     */
+    getParentRouteObject() {
+        var obj = this.data;
+        for (var i = 0; i < this.route.length - 1; i++) {
+            obj = obj[this.route[i]];
+        }
+        return obj;
+    }
+
 }
